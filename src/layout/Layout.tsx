37bb83555d539509f4d5bc55b31f3d8a2730670c
abdessamad10bouih <bouihabdessamad89@gmail.com>
2024-11-25 00:14:25 +0100

    add navbar, public folder, cha,ge tailwind setting

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,13 @@
 import Footer from "@/components/Footer"
 import Navbar from "@/components/Navbar"
-import {TopBar} from "@/components/top-bar"
 import Topbar from "@/components/TopBar"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
     return (
         <div className="min-h-dvh flex-col flex">
-            <TopBar/>
-            <Navbar />
             <Topbar/>
+            <Navbar />
             <div className="flex-1">
                 <Outlet />
             </div>
