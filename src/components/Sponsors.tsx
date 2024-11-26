2b6988466f0ae5677bc75d6fe22a6b1d48dd2ba1
Ouhssini <ahmedouhssini@gmail.com>
2024-11-26 15:14:20 +0100

    --fix the logos in sponser section

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -4,8 +4,8 @@ import SectionHeader from "./SectionHeader"
 const sponsors: string[] = [
     "./brand-01.png",
     "./brand-02.png",
-    "./brand-03.jpg",
     "./brand-01.png",
+    "./brand-02.png",
 ]
 
 const Sponsors = () => {
@@ -13,7 +13,7 @@ const Sponsors = () => {
         <div>
             <SectionHeader title="Our Sponsors" description="We are proud to work with the following companies and organizations." />
 
-            <div className="grid grid-cols-2 md:grid-cols-3  lg:grid-cols-4 items-center justify-center content-center">
+            <div className="grid grid-cols-2 md:grid-cols-3  lg:grid-cols-4 items-center justify-center content-center gap-3">
                     {sponsors.map((sponsor,index)=>{
                        return <img src={sponsor} key={index} className={`mx-auto max-w-28`} />
                     }) }
